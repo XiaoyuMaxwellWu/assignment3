7590592621b931cb955f466a92c1ba91da9a7d58
maxwellxiaoyuwu <xiaoyu.wu@northeastern.edu>
2022-05-01 17:05:32 -0700

    upload image bug fix

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -3,7 +3,6 @@ import express from 'express';
 import { isAuth } from '../utils.js';
 
 const uploadRouter = express.Router();
-
 const storage = multer.diskStorage({
   destination(req, file, cb) {
     cb(null, 'uploads/');
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -49,7 +49,6 @@ export default function ProductEditScreen(props) {
   }, [product, dispatch, productId, successUpdate, navigate]);
   const submitHandler = (e) => {
     e.preventDefault();
-    // TODO: dispatch update product
     dispatch(
       updateProduct({
         _id: productId,
